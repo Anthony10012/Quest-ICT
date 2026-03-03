46ac77d686646bef6a8f9f0f1ab7b3f8c9233c47
Anthony Simond <pv22nbo@eduvaud.ch>
2026-03-03 14:29:38 +0100

    feat: Add main loop and Pseudo validation

--- src/ui/login_page.py
+++ src/ui/login_page.py
@@ -32,3 +32,31 @@ def draw_text_centered(text, font, color,y):
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect(center=(WIDTH //2, y))
     screen.blit(text_surface, text_rect)
+
+def main():
+    global username, input_active, error_msg
+
+    running = True
+    while running:
+        screen.fill(BG_COLOR)
+
+        #--- Events Mangement ---
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    username = username[:-1]
+                elif event.key == pygame.K_RETURN:
+                    # Pseudo validation
+                    if len(username.strip()) < 3:
+                        error_msg = "Le pseudo doit contenir au moins 3 caractères"
+                    else:
+                        print(f"Connexion réussie: {username}")
+                        running = False
+
+                else:
+                    if len(username) < 50:
+                        username += event.unicode
