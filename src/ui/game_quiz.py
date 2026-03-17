31f2edbf99f571cd1d80f19c5d9d1a8339c534fe
Anthony Simond <pv22nbo@eduvaud.ch>
2026-03-17 08:23:01 +0100

    feat: Add Game status and fonts, colors

--- src/ui/game_quiz.py
+++ src/ui/game_quiz.py
@@ -5,7 +5,7 @@ def run_game_menu(screen,quiz_data):
     WIDTH, HEIGHT = screen.get_size()
     clock = pygame.time.Clock()
 
-    # Couleurs et Polices
+    # Colors and Fonts
     BG_COLOR = (66, 37, 112)
     WHITE = (255, 255, 255)
     GREEN = (16, 185, 129)
@@ -13,3 +13,10 @@ def run_game_menu(screen,quiz_data):
 
     font_q = pygame.font.SysFont("Arial", 40, bold=True)
     font_btn = pygame.font.SysFont("Arial", 30)
+
+    #  Game Status
+    current_question_index = 0
+    score = 0
+    time_limit = 20000 # 20 seconds
+    start_time = pygame.time.get_ticks()
+
