2979a560a457964666a99854e98d661a09a7c1b4
Anthony Simond <pv22nbo@eduvaud.ch>
2026-03-17 08:30:56 +0100

    feat: Times is up + Drawing

--- src/ui/game_quiz.py
+++ src/ui/game_quiz.py
@@ -11,7 +11,7 @@ def run_game_menu(screen,quiz_data):
     GREEN = (16, 185, 129)
     RED = (239, 68, 68)
 
-    font_q = pygame.font.SysFont("Arial", 40, bold=True)
+    font_question = pygame.font.SysFont("Arial", 40, bold=True)
     font_btn = pygame.font.SysFont("Arial", 30)
 
     #  Game Status
@@ -19,4 +19,37 @@ def run_game_menu(screen,quiz_data):
     score = 0
     time_limit = 20000 # 20 seconds
     start_time = pygame.time.get_ticks()
+
+    while current_question_index < len(quiz_data):
+        question_data = quiz_data[current_question_index]
+        elapsed = pygame.time.get_ticks() - start_time
+        remaining = max(0, (time_limit - elapsed) // 1000)
+
+        # if times is up
+        if elapsed > time_limit:
+            current_question_index += 1
+            start_time = pygame.time.get_ticks()
+            continue
+
+        screen.fill(BG_COLOR)
+
+        # -- Drawing ---
+        # Text question
+        text_surface = font_question.render(question_data["question"], True, WHITE)
+        screen.blit(text_surface, (100,100))
+
+        # Timer
+        timer_surface = font_question.render(f"Temps : {remaining}", True, WHITE)
+        screen.blit(timer_surface, (WIDTH - 250, 50))
+
+        # Answers button
+        answers_rectangles = []
+        for i, answer in enumerate(question_data["answers"]):
+            rectangle = pygame.Rect(150, 300 + i * 120, 900, 80)
+            pygame.draw.rect(screen,(255, 255, 255, 50), rectangle, border_radius=15)
+            txt = font_btn.render(answer["response_text"], True, WHITE)
+            screen.blit(txt,(180, 320 + i * 120))
+            answers_rectangles.append((rectangle,answer["is_correct"]))
+
+
 
